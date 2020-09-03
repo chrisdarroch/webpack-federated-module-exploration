65c13524962e8ca5ac9caa939ae75beb02251b7c
Christopher Darroch <cdarroch@atlassian.com>
2020-09-03 16:46:23 +0200

    fix: render standard DOM stuff in React.

--- packages/parentapp/src/app.js
+++ packages/parentapp/src/app.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 import AppOne from 'remote-reactapp-one';
@@ -6,12 +6,21 @@ import AppTwo from 'remote-reactapp-two/the-app-contents';
 import three from 'remote-jqueryapp/app-fn';
 
 const App = () => {
-    useEffect(() => three(), []);
+    const externalAppCode = useRef(null);
+
+    useEffect(() => {
+        const el = three();
+        externalAppCode.current.appendChild(el);
+    }, []);
 
     return (
         <div>
             <AppOne />
             <AppTwo />
+            
+            <section ref={externalAppCode}>
+                <p>Other app HTML...</p>
+            </section>
         </div>
     )
 };
